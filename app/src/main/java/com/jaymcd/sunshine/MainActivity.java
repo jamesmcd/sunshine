bf1fe509ae67b7fe26e376c365d7356f6197f434
jmcdermott84 <jmcdermott@quark.com>
2015-02-11 17:23:48 +0000

    ArrayList Dummy Data

--- app/src/main/java/com/jaymcd/sunshine/MainActivity.java
+++ app/src/main/java/com/jaymcd/sunshine/MainActivity.java
@@ -1,15 +1,16 @@
 package com.jaymcd.sunshine;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -60,6 +61,13 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            String[] forecastArray={"Today - Sunny - 88/63","Tomorrow - Foggy - 70/46",
+                    "Weds - Cloudy - 72/63","Thurs - Rainy - 64/51","Fri - Foggy - 70/46","Sat - Sunny - 76/68"};
+
+            ArrayList<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
+
+
             return rootView;
         }
     }
