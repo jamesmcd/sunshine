db6b3125f7732c81e288ac90a616f204742cfab6
jamesmcd <jamesmcd05@gmail.com>
2015-02-14 10:33:07 +0000

    Fixed fragment issues, wrong import

--- app/src/main/java/com/jaymcd/sunshine/ForecastFragment.java
+++ app/src/main/java/com/jaymcd/sunshine/ForecastFragment.java
@@ -1,8 +1,9 @@
 package com.jaymcd.sunshine;
 
-import android.app.Fragment;
+
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
--- app/src/main/java/com/jaymcd/sunshine/MainActivity.java
+++ app/src/main/java/com/jaymcd/sunshine/MainActivity.java
@@ -8,13 +8,13 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
-    ForecastFragment f = new ForecastFragment();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
-           //getSupportFragmentManager().beginTransaction().add(R.id.container, f).commit();
+           getSupportFragmentManager().beginTransaction().add(R.id.container, new ForecastFragment()).commit();
+
         }
     }
 
