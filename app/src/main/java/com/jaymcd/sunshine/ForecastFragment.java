6ef83a35780c283145cf3ef8bac85889076a5d4c
jamesmcd <jamesmcd05@gmail.com>
2015-02-16 19:55:33 +0000

    Internet permissions updated

--- app/src/main/java/com/jaymcd/sunshine/ForecastFragment.java
+++ app/src/main/java/com/jaymcd/sunshine/ForecastFragment.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -25,25 +28,51 @@ import java.util.Arrays;
  */
 public class ForecastFragment extends Fragment {
 
+    public ForecastFragment(){
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+
+        setHasOptionsMenu(true);
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.forecastfragment, menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+         if (id == R.id.action_refresh) {
+            FetchWeatherTask weatherTask = new FetchWeatherTask();
+            weatherTask.execute();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
         String[] forecastArray = {"Today - Sunny - 88/63", "Tomorrow - Foggy - 70/46",
                 "Weds - Cloudy - 72/63", "Thurs - Rainy - 64/51", "Fri - Foggy - 70/46", "Sat - Sunny - 76/68"};
 
         ArrayList<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
-        weekForecast.add("Hi Aurelian");
         ArrayAdapter<String> list = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, weekForecast);
 
         ListView lv = (ListView) rootView.findViewById(R.id.listview_forecast);
         lv.setAdapter(list);
-
-
-
-
 
         return rootView;
     }
@@ -118,7 +147,11 @@ public class ForecastFragment extends Fragment {
             }
             return null;
         }
+
+
     }
+
+
 }
 
 
