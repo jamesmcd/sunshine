e9801603380cc0a3ead50b69c740e66eda4bf8ca
jamesmcd <jamesmcd05@gmail.com>
2015-02-19 20:26:09 +0000

    Lesson 3 started

--- app/src/main/java/com/jaymcd/sunshine/ForecastFragment.java
+++ app/src/main/java/com/jaymcd/sunshine/ForecastFragment.java
@@ -13,8 +13,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -76,11 +78,19 @@ public class ForecastFragment extends Fragment {
         String[] forecastArray = {"Today - Sunny - 88/63", "Tomorrow - Foggy - 70/46",
                 "Weds - Cloudy - 72/63", "Thurs - Rainy - 64/51", "Fri - Foggy - 70/46", "Sat - Sunny - 76/68"};
 
-        List<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
+        final List<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
         mForecastAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, weekForecast);
 
         ListView lv = (ListView) rootView.findViewById(R.id.listview_forecast);
         lv.setAdapter(mForecastAdapter);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String forecast = mForecastAdapter.getItem(position);
+
+                Toast.makeText(getActivity(),forecast,Toast.LENGTH_SHORT).show();
+            }
+        });
 
         return rootView;
     }
